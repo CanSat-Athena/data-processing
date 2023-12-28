ef79bf34605e48bf5a66f7d3d88c3e2ff5657909
Nish Patkar <patkarnishad9@gmail.com>
2023-12-28 02:07:50 +0000

    Added readingValue.txt Added basic parsing code

--- plottingScatterGraph.py
+++ plottingScatterGraph.py
@@ -1,3 +1,32 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
+dataName = []
+dataValue = []
+dataTime = []
+
+valuesFile = open("readingValues.txt", "r")
+
+
+def parseData(valuefile):
+    for line in valuefile:
+        i = 1
+
+        for word in line.split():
+            if i == 1:
+                dataName.append(word)
+            if i == 2:
+                dataValue.append(word)
+            if i == 3:
+                dataTime.append(word)
+            i += 1
+
+
+parseData(valuesFile)
+
+for name in dataName:
+    print(name)
+for value in dataValue:
+    print(value)
+for time in dataTime:
+    print(time)
