ac478bae9b1f2f02c2ea35754a4faddd258de5ae
NishPatkar <120325601+NishPatkar@users.noreply.github.com>
2024-02-14 05:14:23 +0000

    Update parseDataIntoDataBase.py

--- parseDataIntoDataBase.py
+++ parseDataIntoDataBase.py
@@ -1,149 +1,127 @@
-import os
 import sqlite3 as sql
-import json
-import time as sleep
+import json as json
+
+def get_table_name(sensor_type):
+    regular_sensors = ["dht", "bme", "anemometer", "gps", "computed"]
+    for regular_sensor_name in  regular_sensors: 
+        if sensor_type == regular_sensor_name:
+            return "Regular Table"
+    return "IMU Table"
 
 
-def make_database(database_name):
+def updateAverageTable(reading_type, conn, timestamp):
+     cursor = conn.cursor()
+     add_row_sql = ''' INSERT INTO Average_Readings(Reading_Name, Reading_Value,Timestamp)
+                  VALUES(?,?,?) '''
+
+     get_previous_sql = '''SELECT Reading_Value FROM Regular_Sensors WHERE Reading_Name = ?'''
+     cursor.execute(get_previous_sql, (reading_type,))
+     conn.commit()
+     previous_value_list = list(cursor.fetchall())
+     conn.commit()
+
+     previous_values = []
+     for items in previous_value_list:
+        for item in items:
+          previous_values.append(item)
+     averageValue = sum(previous_values)/len(previous_values)
+
+     cursor.execute(add_row_sql, (reading_type, averageValue, timestamp))
+          
+
+def updateRegularTable(reading_type, reading_value, timestamp, reading_seperation_accounter, conn):
+          cursor = conn.cursor()
+
+          add_row_sql = ''' INSERT INTO Regular_Sensors(Reading_Name,Reading_Value,Timestamp)
+                       VALUES(?,?,?) '''
+          cursor.execute(add_row_sql, (reading_type, reading_value,timestamp - reading_seperation_accounter))
+          conn.commit()
+          print("Updated table")
+
+def updateIMUTable(reading_type, reading_value, timestamp, reading_seperation_accounter, conn, json_data, sensor_type):
+          cursor = conn.cursor()
+
+          add_row_sql = ''' INSERT INTO IMU_Sensors(Reading_Name,Reading_Value,Timestamp)
+                       VALUES(?,?,?) '''
+          print(reading_value)
+          print("H")
+          for value in reading_value:
+             print(value)
+             cursor.execute(add_row_sql, (reading_type, value,timestamp - reading_seperation_accounter))
+             conn.commit()
+             print("Updated table")
+
+
+def calculate_seperation_time(reading_type):
+    json_frequency = 1000
+    seperation_time = json_frequency/len(reading_type)
+    return seperation_time
+
+def parse_data(conn, json_file, numberOfObjectsParsed):    
+
+    table_name = ""
+    timestamp = int
+    reading_seperation = int
+    reading_seperation_accounter = 0
+
+    make_average_data = ["dht_temp", "dht_humidity", "bme_temp", "bme_humidity", "bme_pressure", "ppm", "airQuality", "windSpeed", "speed"]
+
+    
+    timestamp = json_file[numberOfObjectsParsed]["timestamp"]["time"][0]
+    for sensor_type in json_file[numberOfObjectsParsed]:
+        table_name = get_table_name(sensor_type)
+            
+        for reading_type in json_file[numberOfObjectsParsed][sensor_type]:
+            reading_seperation = calculate_seperation_time(reading_type)
+            print(reading_type)
+            for reading_value in json_file[numberOfObjectsParsed][sensor_type][reading_type]:
+                    reading_seperation_accounter += reading_seperation
+                    if table_name == "Regular Table":
+                        updateRegularTable(reading_type, reading_value, timestamp, reading_seperation_accounter, conn)
+                    elif reading_type != "time":
+                        print(reading_value)
+                        updateIMUTable(reading_type, reading_value, timestamp, reading_seperation_accounter, conn,sensor_type)
+                    if reading_type in make_average_data:
+                         updateAverageTable(reading_type,conn,timestamp)
+
+    
+def connect_to_database(database_name):
+
     conn = None
 
-    try:
+    try: 
         conn = sql.connect(database_name)
         return conn
     except sql.Error as e:
         print(e)
         return conn
 
+def create_table(conn):
 
-def does_database_exist(file_name):
-    data_base_exists = False
+    cur = conn.cursor()
 
-    if os.path.isfile(file_name):
-        data_base_exists = True
-    return data_base_exists
+    sql_command_IMU = '''CREATE TABLE IF NOT EXISTS IMU_Sensors(Reading_Name TEXT, Reading_Value REAL, Timestamp INT)'''
+    sql_command_Regular = '''CREATE TABLE IF NOT EXISTS Regular_Sensors(Reading_Name TEXT, Reading_Value REAL, Timestamp INT)'''
+    sql_command_Average = '''CREATE TABLE IF NOT EXISTS Average_Readings(Reading_Name TEXT, Reading_Value REAL, Timestamp INT)'''
 
-
-def create_table(connection):
-    cursor = connection.cursor()
-
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS imuSensors( Data_Type TEXT,Reading_Value REAL,Timestamps INTEGER )""")
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS regularSensors( Data_Type TEXT, Reading_Value REAL , Timestamps INTEGER )""")
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS averageData( Data_Type TEXT, Reading_Value_Average REAL , Timestamps INTEGER )""")
-
-
-
-def get_table_name(reading_header):
-    imu_header_name = ["imu"]
-    regular_header_name = ["timestamp", "dht", "bme", "anemometer", "gps"]
-    table_name = None
-
-    if reading_header in imu_header_name:
-        table_name = "imuSensors"
-    elif reading_header in regular_header_name:
-        table_name = "regularSensors"
-
-    return table_name
-
-def update_regular_table( connection,data_type,reading_value, reading_time):
-          cursor = connection.cursor()
-
-
-          add_row_sql = ''' INSERT INTO regularSensors(Data_Type,Reading_Value,Timestamps)
-                       VALUES(?,?,?) '''
-          cursor.execute(add_row_sql, (data_type, reading_value,reading_time))
-          connection.commit()
-          print("Updated table")
-
-
-def update_imu_table(connection, data_type, reading_value, reading_time):
-    cursor = connection.cursor()
-
-    add_row_sql = ''' INSERT INTO imuSensors(Data_Type, Reading_Value,Timestamps)
-                     VALUES(?,?,?) '''
-
-    for item in reading_value:
-        cursor.execute(add_row_sql, (data_type, item , reading_time))
-        connection.commit()
-    print("Updated table")
-
-def update_average_table(connection, data_type, reading_time):
-    cursor = connection.cursor()
-
-    add_row_sql = ''' INSERT INTO averageData(Data_Type, Reading_Value_Average,Timestamps)
-                  VALUES(?,?,?) '''
-
-    get_previous_sql = '''SELECT Reading_Value FROM regularSensors WHERE Data_Type = ?'''
-    cursor.execute(get_previous_sql, (data_type,))
-    connection.commit()
-    previous_value_list = list(cursor.fetchall())
-    connection.commit()
-    previous_values = []
-    for item in previous_value_list:
-
-        for value in item:
-            print(value)
-            previous_values.append(value)
-
-    print(len(previous_values))
-    average_value = sum(previous_values) / len(previous_values)
-    print("Average Value")
-    print(average_value)
-
-
-    cursor.execute(add_row_sql, (data_type, average_value , reading_time))
-    connection.commit()
-    print("Updated table")
-
-
-def calculate_time(json_file, data_type, data_header):
-    number_of_readings = len(json_file[data_header][data_type])
-    reading_frequency = 1 / number_of_readings
-    return reading_frequency
-def parse_data(connection, json_data):
-    cursor = connection.cursor()
-    make_average_data = ["dht_temp", "dht_humidity", "bme_temp", "bme_humidity", "bme_pressure", "ppm", "airQuality", "windSpeed", "speed"]
-
-    for dataHeaders in json_data:
-        reading_header = dataHeaders
-        table_name = get_table_name(reading_header)
-        if table_name is not None:
-            for dataType in json_data[dataHeaders]:
-                reading_frequency = calculate_time(json_data, dataType, reading_header)
-                package_time = json_data["timestamp"]["time"][0]
-                for reading_value in json_data[dataHeaders][dataType]:
-                    package_time -= reading_frequency
-                    if(table_name == "regularSensors"):
-                        update_regular_table( connection, dataType, reading_value, package_time)
-                        if dataType in make_average_data:
-                            update_average_table(connection, dataType, package_time)
-                    elif(table_name == "imuSensors"):
-                        update_imu_table( connection,  dataType, reading_value, package_time)
-
-
-
+    cur.execute(sql_command_IMU)
+    cur.execute(sql_command_Regular)
+    cur.execute(sql_command_Average)
+    
+    conn.commit()
+    print("Tables Found/Created")
+    
 
 def main():
-    data_base_name = "CanSatDatabase.db"
-    connection = None
 
-    if not does_database_exist(data_base_name):
-        connection = make_database(data_base_name)
-        print("Database Made!")
-    else:
-        print("Database Found!")
-        connection = sql.connect(data_base_name)
-    if connection is not None:
-        create_table(connection)
+    numberOfObjectsParsed = 0
 
-    while 1 == 1:
-        jsonFile = open("dummyReadingsJSON.json")
-        jsonData = json.load(jsonFile)
+    json_file = open("dummyReadings.json")
+    json_data = json.load(json_file)
+    conn = connect_to_database("cansatReadings.db")
+    create_table(conn)
+    parse_data(conn, json_data)
 
-        parse_data(connection, jsonData)
-        sleep.sleep(1)
 
 if __name__ == '__main__':
-    main()
+    main()   
