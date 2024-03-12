16d1e1f7dc904ee0d58d1eb26d76dc77df3100d4
NishPatkar <120325601+NishPatkar@users.noreply.github.com>
2024-03-12 02:09:10 +0000

    Add files via upload

--- dataParser.py
+++ dataParser.py
@@ -4,7 +4,7 @@ from datetime import timezone, datetime
 import os
 
 
-global_data_list = [[],[],[],[]]
+global_data_list = []
 previous_rows = []
 
 
@@ -15,52 +15,40 @@ def create_connection(data_text_file):
 def row_to_array():
     with open("readings.csv",'r') as csv_file:
         reader = csv.reader(csv_file)
-        in_section = False
-        section_counter = 0
-        data_counter = 0
-        current_row_number = 0
         for row in reader:
-            if row not in previous_rows:
-             in_section = False
-             section_counter = 0
-             data_counter = 0
-             for data in row:
-                value = data
-                if '[' in data:
-                    value = data[1:]
-                    in_section = True
-                    section_counter += 1
-                if ']' in data:
-                    value = data[:-1]
-                    in_section = False
-                    array_index = get_array_index(section_counter, data_counter)
-                    global_data_list[array_index].append(value)
-                if in_section:
-                    array_index = get_array_index(section_counter, data_counter)
-                    global_data_list[array_index].append(value)
-                    data_counter +=1
-            previous_rows.append(row)
-    print(global_data_list)
+            inData = False
+            element_counter = 0
+            if row not in previous_rows: 
+                print(row)
+                for data in row:
+                    value = data
+                    if ':' in data:
+                        inData = True
+                        characterIndex = data.index(':') + 1
+                        value = data[characterIndex:]
+                    if inData:
+                        print(element_counter)
+                        print(value)
+                        global_data_list.append(value)
+                        element_counter+=1
+                previous_rows.append(row)
+            
 
 def create_tables(connection):
     cursor = connection.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS cansatReadings(dataType STRING, dataValue REAl, Timestamp REAL)''')
     connection.commit()
     
-def parse_data(connection, row_number):
-    section_counter = 0
-    new_row_number = 0
+def parse_data(connection):
     time_unix = int(datetime.now(tz=timezone.utc).timestamp() * 1000)
-    data_names = ["latitiude", "longitude", "temp", "pressure"]
+    data_names = ["latitiude", "longitude","alt", "temp", "pressure"]
     cursor = connection.cursor()
-    for section in global_data_list:
-        for value in section:
-            reading_name = data_names[section_counter]
+    for value in global_data_list:
+            reading_name = data_names[global_data_list.index(value)]
             cursor.execute('''INSERT INTO cansatReadings(dataType, dataValue, Timestamp) VALUES(?,?,?)''',( reading_name,value,time_unix))
             connection.commit()
-        section_counter+=1
-    for section in global_data_list:
-        section.clear()
+            
+    global_data_list.clear()
 
 def get_array_index(section_counter, data_counter):
     gps_data = [0,1]
@@ -92,7 +80,7 @@ while True:
     t = os.path.getmtime(fn)
     if t != prev_time:
         row_to_array()
-        parse_data(connection, row_number)
+        parse_data(connection)
         
         print("Updated")
         prev_time = t
